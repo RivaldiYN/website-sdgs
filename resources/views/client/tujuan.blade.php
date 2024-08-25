369bd71afbdc00a28fb77ba2f4a3a7d1ff84bf73
yondikavl <yondikaviolanda@gmail.com>
2024-08-25 13:03:25 +0700

    fix: tujuan

--- resources/views/client/tujuan.blade.php
+++ resources/views/client/tujuan.blade.php
@@ -42,6 +42,12 @@
                 display: none !important;
             }
         }
+
+        @media (max-width: 375px) {
+            .svg-atas {
+                display: none;
+            }
+        }
     </style>
 @endsection
 
